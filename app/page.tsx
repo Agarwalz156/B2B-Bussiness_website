00744ea24564025488b56423a82b4e9aaeee4f33
AcerNitro <muditagarwalz156@gmail.com>
2026-04-20 21:47:04 +0530

    fix Leadform importerror

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import LeadForm from '@/components/LeadForm';
+import { LeadForm } from '@/components/LeadForm';
 import { ServiceCard } from '@/components/ServiceCard';
 import { WhatsAppButton } from '@/components/WhatsAppButton';
 import { ShimmerButton } from '@/components/ShimmerButton';
